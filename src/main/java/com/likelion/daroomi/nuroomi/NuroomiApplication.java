4e285f50a00de630a309e602a238e30060291ca0
권진한 <ymyhjh2@naver.com>
2023-08-09 20:38:44 +0900

    first init

---
+++ src/main/java/com/likelion/daroomi/nuroomi/NuroomiApplication.java
@@ -0,0 +1,13 @@
+package com.likelion.daroomi.nuroomi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NuroomiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NuroomiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/likelion/daroomi/nuroomi/NuroomiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likelion.daroomi.nuroomi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NuroomiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
